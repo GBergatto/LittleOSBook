7da108414b3f214a4641f69adf03ea4240df66cb
GBergatto <gb.bergatto@gmail.com>
2024-07-21 17:18:05 +0200

    Implement kernel stack

--- loader.s
+++ loader.s
@@ -1,18 +1,36 @@
+extern kmain
+
 global loader ; the entry symbol for ELF
 
-MAGIC_NUMBER equ 0x1BADB002 ; define the magic number constant
-FLAGS equ 0x0 ; multiboot flags
-CHECKSUM equ -MAGIC_NUMBER ; calculate the checksum
-                           ; (magic number + checksum + flags should equal 0)
+KERNEL_STACK_SIZE equ 4096    ; size of stack in bytes
 
-section .text: ; start of the text (code) section
-align 4 ; the code must be 4 byte aligned
-   dd MAGIC_NUMBER ; write the magic number to the machine code,
-   dd FLAGS ; the flags,
-   dd CHECKSUM ; and the checksum
+MAGIC_NUMBER equ 0x1BADB002   ; define the magic number constant
+FLAGS equ 0x0                 ; multiboot flags
+CHECKSUM equ -MAGIC_NUMBER    ; calculate the checksum
+                              ; (magic number + checksum + flags should equal 0)
+
+; reserve memory for the kernel stack
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
+
+section .text:
+align 4
+   dd MAGIC_NUMBER
+   dd FLAGS
+   dd CHECKSUM
+
+; point esp to the start of the stack
+mov esp, kernel_stack + KERNEL_STACK_SIZE
 
 loader: ; entrypoint in linker script
    mov eax, 0xCAFEBABE
+
+   push dword 3
+   push dword 2
+   push dword 1
+   call kmain
 
 .loop: 
    jmp .loop
